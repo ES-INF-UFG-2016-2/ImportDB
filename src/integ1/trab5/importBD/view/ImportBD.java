22945ac5425578d26c19d6d1fcea7fab75debe83
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-07-23 20:47:46 -0300

    Exception agora é lançada com sucesso para o usuário

--- src/integ1/trab5/importBD/view/ImportBD.java
+++ src/integ1/trab5/importBD/view/ImportBD.java
@@ -5,29 +5,29 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class ImportBD {
-
-	public static void main (String[] args) {
-
-		System.out.println("==================== ImportBD =====================\n"
-		                 + "Sistema de importação de dados para Bancos de dados");
-
-		try {
-			
-		} catch(Exception e) {
-			System.out.println("EXCECAO: " + e.getMesssage());
-		}
-	}
-	
-	private static void recebeDados(String[] args) throws IllegalArgumentException, FileNotFoundException {
-		System.out.println(args[0]);
-		if (args.length == 1) {
-			
-			String content = new Scanner(new File(args[0])).useDelimiter("\\Z").next();
-			System.out.println(content);
-		}
-		else {
-			throw new IllegalArgumentException("Nenhum arquivo foi passado como "
-							 + "parâmetro para o programa");
-		}
-	}
+    
+    public static void main(String[] args) {
+        
+        System.out.println("==================== ImportBD =====================\n"
+                + "Sistema de importação de dados para Bancos de dados");
+        
+        try {
+            recebeDados(args);
+        }
+        catch (IllegalArgumentException | FileNotFoundException e) {
+            System.err.println("EXCEÇÃO: " + e.getLocalizedMessage());
+        }
+    }
+    
+    private static void recebeDados(String[] args) throws IllegalArgumentException, FileNotFoundException {
+        if (args.length == 1) {
+            System.out.println("Arquivo:" + args[0]);
+            String content = new Scanner(new File(args[0])).useDelimiter("\\Z").next();
+            System.out.println(content);
+        }
+        else {
+            throw new IllegalArgumentException("Nenhum arquivo foi passado como "
+                    + "parâmetro para o programa");
+        }
+    }
 }
