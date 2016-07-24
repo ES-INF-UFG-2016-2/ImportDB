8807fbd3419842db46ef907b36ab826a9867f16e
Gustavo Moraes <gustavosotnas1@gmail.com>
2016-07-24 10:31:18 -0300

    Revert "Feito pequeno teste para exibir os registros na tela."
    This reverts commit bb7e18f8723df108008768a9c35c7c32a821a659.

--- src/integ1/trab5/importBD/view/ImportBD.java
+++ src/integ1/trab5/importBD/view/ImportBD.java
@@ -3,14 +3,10 @@ package integ1.trab5.importBD.view;
 import java.io.IOException;
 
 import integ1.trab5.importBD.controller.IOController;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class ImportBD {
     
     public static String NOME_ARQUIVO_TEXTO_DEFAULT = "Egressos-para-Importar.txt";
-    /** Internal Field Separator (IFS) */
-    private static final String IFS = "\\\\"; // regex "\"
     
     public static void main(String[] args) {
         
@@ -23,34 +19,9 @@ public class ImportBD {
         {
             arquivo = IOController.recebeDados(args);
             IOController.validaConsistenciaDados(arquivo);
-            
-            ArrayList<String[]> registros = parseCSV(arquivo);
-
-            for (int i=0; i<registros.size(); i++) {
-                for (int j=0; j<registros.get(i).length; j++) {
-                    System.out.println(registros.get(i)[j]);
-                }
-                System.out.println(); 
-            }
         }
         catch (IllegalArgumentException | IOException e) {
             System.err.println("EXCEÇÃO: " + e.getLocalizedMessage());
         }
-    }
-    
-    private static ArrayList<String[]> parseCSV(String csvFileContent) throws IOException {
-
-        Scanner scanner = new Scanner(csvFileContent);
-        String line = "";
-        ArrayList<String[]> result = new ArrayList<>();
-        
-        while (scanner.hasNextLine()) {
-
-            line = scanner.nextLine();
-            String[] registro = line.split(IFS);
-            result.add(registro);
-        }
-        scanner.close();
-        return result;
     }
 }
